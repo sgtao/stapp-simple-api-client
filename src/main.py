9b1fa7255ac89541852f10de9d4eccfd4def90ac
Shogo Ogami <sg.tao.so@gmail.com>
2025-03-29 12:54:41 +0900

    Del:update pages for remove example app page

--- src/main.py
+++ src/main.py
@@ -8,4 +8,8 @@ Edit `/src` and `/tests` to customize this app to your heart's desire :heart:.
 
 # サイドバーのページに移動
 # st.page_link("pages/example_app.py", label="Go to Example App")
-st.page_link("pages/01_example_app.py", label="Go to Example App", icon="🚀")
+st.page_link(
+    "pages/11_trial_simple_api.py",
+    label="Go to trial_simple_api App",
+    icon="🚀",
+)
