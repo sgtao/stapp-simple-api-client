8586622f5d2b4774d967ef74518002e16ec9d83c
Shogo Ogami <sg.tao.so@gmail.com>
2025-04-07 07:32:41 +0900

    Add:update ApiResponseViewer component for show content other than application/json

--- src/components/ApiResponseViewer.py
+++ src/components/ApiResponseViewer.py
@@ -52,6 +52,29 @@ def extract_property_from_json(json_data, property_path):
 
 
 class ApiResponseViewer:
+    def __init__(self):
+        pass
+
+    def response_content(self, response):
+        content_type = response.headers.get("Content-Type", "")
+        if "application/json" in content_type:
+            return "application/json"
+        elif "text/html" in content_type:
+            return "text/html"
+        elif "text/plain" in content_type:
+            return "text/plain"
+        else:
+            return "unsupported"
+
+    def header_viewer(self, response):
+        with st.expander("レスポンスヘッダー"):
+            try:
+                # 辞書形式のヘッダーをJSONとして表示
+                st.json(dict(response.headers))
+            except Exception as e:
+                st.error(
+                    f"レスポンスヘッダーの表示中にエラーが発生しました: {str(e)}"
+                )
 
     def render_extracted_value(self, extracted_value):
         """抽出された値をタブで表示する"""
@@ -86,20 +109,21 @@ class ApiResponseViewer:
     def render_viewer(self, response):
         # st.text(response.status_code)
         st.metric(label="Status Code", value=response.status_code)
-
+        content_type = self.response_content(response)
         if st.session_state.user_property_path != "":
-            response_json = response.json()  # JSON形式の場合
-            property_path = st.session_state.user_property_path
-            # 抽出したいプロパティの指定
             try:
-                # property_value =
-                #     response_json["completion"]["tags"][0]["value"]
-                # property_value = response_json["tags"][0]
-                # st.write(f"Extracted Value: {property_value}")
+                # 抽出したいプロパティの指定
+                property_path = st.session_state.user_property_path
 
-                extracted_value = extract_property_from_json(
-                    response_json, property_path
-                )
+                if content_type == "application/json":
+                    response_json = response.json()  # JSON形式の場合
+                    extracted_value = extract_property_from_json(
+                        response_json, property_path
+                    )
+                else:
+                    # extracted_value = response.body
+                    extracted_value = response.text
+
                 # 抽出された値を表示
                 if extracted_value is not None:
                     st.success(f"Extracted Value({property_path}): Found.")
@@ -113,16 +137,15 @@ class ApiResponseViewer:
             except TypeError:
                 st.error("プロパティの型が想定と異なります。")
 
-        with st.expander("レスポンスヘッダー"):
-            try:
-                # 辞書形式のヘッダーをJSONとして表示
-                st.json(dict(response.headers))
-            except Exception as e:
-                st.error(
-                    f"レスポンスヘッダーの表示中にエラーが発生しました: {str(e)}"
-                )
+        # レスポンスの表示
+        self.header_viewer(response)
         with st.expander("レスポンスボディ"):
             try:
-                st.json(response.json())  # JSON形式の場合
+                if content_type == "application/json":
+                    # JSON形式の場合
+                    st.json(response.json())
+                else:
+                    # Text / HTML の場合
+                    st.markdown(response.text)
             except json.JSONDecodeError:
                 st.text(response.text)  # テキスト形式の場合
