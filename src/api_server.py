05a2b3d1024064501c00a83fd9394015458a88e2
Shogo Ogami <sg.tao.so@gmail.com>
2025-04-12 20:50:12 +0900

    Add:update api_server_control page for POST replaced request body with user_inputs

--- src/api_server.py
+++ src/api_server.py
@@ -10,6 +10,9 @@ from fastapi.responses import JSONResponse
 import uvicorn
 import yaml
 import requests
+import urllib.parse
+
+from functions.ApiRequestor import ApiRequestor
 
 # from src.components.ConfigFiles import ConfigFiles
 from components.ConfigFiles import ConfigFiles
@@ -87,11 +90,11 @@ async def execute_service(request: Request):
         )
     config_data = read_yaml_file(config_file_path)
 
-    # 3. Config データへの user_input_* の適用 (プレースホルダ置換)
+    # 2. Config データへの user_input_* の適用 (プレースホルダ置換)
     # processed_config = apply_user_inputs(config_data, user_inputs)
     processed_config = config_data.get("session_state")
 
-    # 4. APIリクエスト情報の取得
+    # 3. APIリクエスト情報の取得
     api_url = processed_config.get("uri")
     method = processed_config.get("method", "GET").upper()
     # headers = processed_config.get("header_df")
@@ -104,16 +107,57 @@ async def execute_service(request: Request):
             status_code=400, detail="Missing 'url' in processed config"
         )
 
+    # 4. コンフィグ情報の置換
+    # number_user_inputs = body_data.get("num_user_inputs")
+    user_inputs = body_data.get("user_inputs")
+    replaced_uri = api_url
+    # print(f"Original URI: {api_url}")
+    print(f"user_inputs: {user_inputs}")
+    # # user_inputs が辞書であることを確認
+    # if isinstance(user_inputs, dict):
+    for key, user_input_value in user_inputs.items():
+        # user_input_value が None でないことを確認
+        if user_input_value is not None:
+            # print(
+            #     f"Replacing placeholder ＜{key}＞ with '{user_input_value}'"
+            # )
+            # URI の置換
+            value_encoded = urllib.parse.quote(str(user_input_value))
+            replaced_uri = replaced_uri.replace(f"＜{key}＞", value_encoded)
+
+    if req_body_template:
+        posted_req_body = req_body_template
+        # print(
+        #     f"""
+        #     {req_body_template}.
+        #     Before replace,
+        #     {posted_req_body}
+        #     """
+        # )
+        for key, user_input_value in user_inputs.items():
+            # user_input_value が None でないことを確認
+            if user_input_value is not None:
+                posted_req_body = posted_req_body.replace(
+                    f"＜{key}＞", user_input_value
+                )
+        req_body_template = posted_req_body
+
     # 5. 外部APIへのリクエスト発行
     try:
-        api_requestor = requests.Session()  # セッションを使うと効率が良い
-        response = api_requestor.request(
+        # api_requestor = requests.Session()  # セッションを使うと効率が良い
+        api_requestor = ApiRequestor()
+        response = api_requestor.send_request(
             method=method,
-            url=api_url,
+            # url=api_url,
+            url=replaced_uri,
             headers=headers,  # processed_config から取得したヘッダー
-            json=(
-                req_body_template if method in ["POST", "PUT"] else None
-            ),  # processed_config から取得したボディ
+            body=(
+                # 4. 置換したリクエストボディ
+                # json.load(req_body_template)
+                req_body_template
+                if method in ["POST", "PUT"]
+                else None
+            ),
         )
         response.raise_for_status()  # HTTPエラーチェック
         api_response_json = response.json()
